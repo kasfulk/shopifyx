78cc9d80aecd4bffe8c9e88b1c011dcd4146bd05
rizface <alfarizzi2711@gmail.com>
2024-03-13 22:34:39 +0700

    restrict access to upload image endpoint

--- api/routes/image.go
+++ api/routes/image.go
@@ -2,10 +2,11 @@ package routes
 
 import (
 	"shopifyx/api/handlers"
+	"shopifyx/api/middleware"
 
 	"github.com/gofiber/fiber/v2"
 )
 
 func ImageRoutes(app *fiber.App, h handlers.ImageUploader) {
-	app.Post("/v1/image", h.Upload)
+	app.Post("/v1/image", h.Upload).Use(middleware.JWTAuth())
 }
