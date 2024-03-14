c0aa634498126ee1f7b9efdac5e0ba588bde4617
kanianursawitri <kaniamalimba@gmail.com>
2024-03-14 14:15:54 +0700

    change unique indexing to combine with userID

--- db/migrations/000005_add_product_index.up.sql
+++ db/migrations/000005_add_product_index.up.sql
@@ -1,5 +1,5 @@
 /*
-add unique index on product name
+add unique index on product name combine with user id
 */
 
-alter table products add constraint unique_name unique (name);
+alter table products add constraint unique_name unique (name, user_id);
